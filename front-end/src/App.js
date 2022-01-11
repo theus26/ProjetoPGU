e1fc4963a50e56e4e901122075be4e145291efea
Matheus <theuslipe26@gmail.com>
2022-01-11 18:34:24 -0300

    confirmar

---
+++ front-end/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Card from './components/card';
+
+function App() {
+  return (
+   <Card />
+  );
+}
+
+export default App;
---
+++ front-end/src/components/card.js
@@ -0,0 +1,37 @@
+import React from "react";
+import './card.css';
+
+export default function card (){
+    return (
+        <div className="container">
+            <div className="container-content">
+                <p id="texto1">Atendimento pelo Whatsapp</p> <br/>
+                <p id="texto2">Segunda a Sexta, das 7h as 13h.</p>
+                <img alt="" src="whatsapp.png"></img>
+                
+                <div className="content1">
+                    <a className = "link" href="https://api.whatsapp.com/send?1=pt_BR&phone=5579991912162"  style={{color: 707070}} >
+                    <br/>
+                    (79) 99191-2162
+                    <br/>
+                    </a>
+                    <span >
+                        Suporte
+                    </span>
+                    <br/>
+                </div>
+                <div className="content2">
+                    <a className="link" href="https://api.whatsapp.com/send?1=pt_BR&phone=5579991912482" style={{color: 707070}}>
+                        (79) 99191-2482
+                        <br/>
+                       
+                    </a>
+                    <span>
+                            Desenvolvimento
+                        </span>
+                </div>
+
+            </div>
+        </div>
+    );
+}
