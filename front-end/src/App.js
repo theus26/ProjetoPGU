052b974b78bb628b8b990d3342db8e798554d456
Matheus <theuslipe26@gmail.com>
2022-01-12 01:15:47 -0300

    confirmar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Card from './components/card';
 import Footer from './components/footer';
-import Login from './pages/login';
+import Login from './pages/login/login';
+
 
 
 function App() {
   return (
    
-   <Login />
+   <Card />
    
   );
 }
--- front-end/src/pages/login/login.js
+++ front-end/src/pages/login/login.js
@@ -1,8 +1,36 @@
 import React from "react";
 import './login.css';
 
+
+
+
 export default function login (){
+  
+   
     return(
-        <h1>Olla</h1>
+        
+        <div className="container-form">
+            <div className="card-Form">
+               <img src='/SGP.png'></img>
+
+                <div className="label-float">
+                    <input type="text" name="usuario" id="usuario" required placeholder="Digite seu Usuario"  maxLength="15" minLength="3" required></input>
+                    <label for="userLabel" id="labelUsuario" >Usuario</label>
+                  
+           </div>
+           <div className="label-float">
+                    <input type="password" name="senha" id="senha" required placeholder="Digite sua Senha" maxLength="25" minLength="8" required></input>
+                    <label for="userLabel" id="labelSenha" >Senha</label>
+                  
+           </div>
+           <button type="submit" className="botao">Entrar</button> <br/><br/>
+           <a id="link-form" href="https://intranet.pge.se.gov.br/Preferencias/esqueceu_senha.aspx">Esqueceu sua senha ou bloqueou seu usuário?</a>
+            </div>
+         
+        </div>
     );
+    
 }
+
+    
+    
